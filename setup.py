b2fcec1c0679a0771f708530ca03a2b800322217
Alex Finnegan <afinneg2@illinois.edu>
2020-05-14 13:41:30 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,4 @@ setup(
     long_description=open('README.md').read(),
     install_requires=['numpy >= 1.18.1', 'scipy >= 1.4.1', 'matplotlib >= 3.2.1', 'pandas >= 1.0.3',
 		      'seaborn >= 0.10.0', "pymc3 >= 3.8" ],
-    entry_points = { 'console_scripts' : ['funny=RoZeroTools.funny:main'] }
 )
